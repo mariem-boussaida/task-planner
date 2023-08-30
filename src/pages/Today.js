e9e88f845188dc88fd0b74b3783bbe6131adf924
boussaida mariem <boussaidamariem@gmail.com>
2023-08-30 19:05:55 +0200

    clandrie

--- src/pages/Today.js
+++ src/pages/Today.js
@@ -1,41 +1,23 @@
-import React from "react";
-import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
-import "@syncfusion/ej2-calendars/styles/material.css";
+import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
-const Today = () => {
-  const data = [
-    {
-      Id: 2,
-      Subject: 'Meeting',
-      StartTime: new Date(2018, 1, 15, 10, 0),
-      EndTime: new Date(2018, 1, 15, 12, 30),
-      IsAllDay: false,
-      Status: 'Completed',
-      Priority: 'High'
-    },
-  ];
-
-  const fieldsData = {
-    id: 'Id',
-    subject: { name: 'Subject' },
-    isAllDay: { name: 'IsAllDay' },
-    startTime: { name: 'StartTime' },
-    endTime: { name: 'EndTime' }
-  };
-
-  const eventSettings = { dataSource: data, fields: fieldsData };
-
+function Today() {
   return (
-    <div className="m-4 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <h1>Calendars</h1>
-      <ScheduleComponent
-        width='100%'
-        height='550px'
-        selectedDate={new Date(2018, 1, 15)}
-        eventSettings={eventSettings}
-      >
-        <Inject services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]} />
-      </ScheduleComponent>
+    <div>
+    <div className='today '></div>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView='dayGridMonth'
+        headerToolbar={{
+          start: 'today prev,next', // sera normalement à gauche, ou à droite en cas de RTL
+          center: 'title',
+          end: 'dayGridMonth,timeGridWeek,timeGridDay' // sera normalement à droite, ou à gauche en cas de RTL
+        }}
+        height={"80vh"}
+      />
     </div>
   );
 }
